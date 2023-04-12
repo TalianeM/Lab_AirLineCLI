b25675a35a49ea7b7ea59a19a6c5e26648f45122
William Dorling <williamdorling@hotmail.com>
2023-04-12 11:18:28 +0100

    added CLI to Airline methods

--- src/main/java/Airline.java
+++ src/main/java/Airline.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Airline {
 
@@ -17,20 +18,86 @@ public class Airline {
         this.flights.add(newFlight);
     }
 
-    public void cancelFlight(int id){
-        for(Flight flight : this.flights){
-            if(flight.getId() == id){
-                this.flights.remove(flight);
-            }
-        }
+
+    public void addFlight(){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please input flight destination");
+        String destination = scanner.nextLine();
+
+        System.out.println(("Please input flight id"));
+        int id = scanner.nextInt();
+
+        Flight newFlight = new Flight(destination, id);
+        this.flights.add(newFlight);
+
     }
+
+
+
+
+
+
+//    public void cancelFlight(int id){
+//        for(Flight flight : this.flights){
+//            if(flight.getId() == id){
+//                this.flights.remove(flight);
+//            }
+//        }
+//    }
+//
+//    public void cancelFlight(){
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please input flight id");
+//        int id = scanner.nextInt();
+//
+//        for(Flight flight : this.flights){
+//            if(flight.getId() == id){
+//                this.flights.remove(flight);
+//            }
+//        }
+//    }
 
     public void cancelFlight(String destination, int id){
         Flight newFlight = new Flight(destination, id);
         this.flights.remove(newFlight);
     }
 
+    public void cancelFlight(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please input flight destination");
+        String destination = scanner.nextLine();
+
+        System.out.println("Please input flight id");
+        int id = scanner.nextInt();
+
+
+        Flight newFlight = new Flight(destination, id);
+        this.flights.remove(newFlight);
+    }
+
+
+
     public void addPassenger(String name, long contactNumber, int id){
+        Passenger newPassenger = new Passenger(name, contactNumber, id);
+        this.passengers.add(newPassenger);
+    }
+
+    public void addPassenger(){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please input passenger name");
+        String name = scanner.nextLine();
+
+        System.out.println("Please input passenger contact number");
+        long contactNumber = scanner.nextLong();
+
+        System.out.println("Please input passenger id");
+        int id = scanner.nextInt();
+
         Passenger newPassenger = new Passenger(name, contactNumber, id);
         this.passengers.add(newPassenger);
     }
@@ -38,24 +105,41 @@ public class Airline {
     public void addPassengerToFlight(Passenger newPassenger,Flight flight){
         flight.addPassenger(newPassenger);
     }
-    public String displayFlights(){
-        for (Flight flight : this.flights){
-            return flight.getDestination();
 
+    public void addPassengerToFlight(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please input passenger id");
+        int passengerId = scanner.nextInt();
+
+        System.out.println("Please input flight id");
+        int flightId = scanner.nextInt();
+
+        Passenger newPassenger = null;
+        for(Passenger passenger : this.passengers){
+            if (passenger.getId() == passengerId){
+                newPassenger = passenger;
+                break;
+            }
         }
-        return null;
+
+        Flight newFlight = null;
+        for(Flight flight :this.flights){
+            if (flight.getId() == flightId){
+                newFlight = flight;
+                break;
+            }
+        }
+
+        newFlight.addPassenger(newPassenger);
+
     }
 
-
-
-
-
-
-
-
-
-
-
+    public void displayFlights(){
+        for (Flight flight : this.flights){
+            System.out.println("Flight number " + flight.getId() + " goes to " + flight.getDestination());
+        }
+    }
 
 
 
@@ -84,4 +168,10 @@ public class Airline {
     public void setName(String name) {
         this.name = name;
     }
+
+
+
+
+
+
 }
