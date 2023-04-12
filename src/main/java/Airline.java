7bee56b071d187ddcf399e5ee341e04f4fdf8719
William Dorling <williamdorling@hotmail.com>
2023-04-12 10:01:25 +0100

    adds Airline class

---
+++ src/main/java/Airline.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+
+public class Airline {
+
+    private ArrayList<Flight> flights;
+    private ArrayList<Passenger> passengers;
+    private String name;
+
+    public Airline(String name){
+        this.name = name;
+        this.passengers = new ArrayList<Passenger>();
+        this.flights = new ArrayList<Flight>();
+    }
+
+    public void addFlight(String destination, int id){
+        Flight newFlight = new Flight(destination, id);
+        this.flights.add(newFlight);
+    }
+
+    public void cancelFlight(int id){
+        for(Flight flight : this.flights){
+            if(flight.getId() == id){
+                this.flights.remove(flight);
+            }
+        }
+    }
+
+    public void cancelFlight(String destination, int id){
+        Flight newFlight = new Flight(destination, id);
+        this.flights.remove(newFlight);
+    }
+
+    public void addPassenger(String name, long contactNumber, int id){
+        Passenger newPassenger = new Passenger(name, contactNumber, id);
+        this.passengers.add(newPassenger);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public ArrayList<Flight> getFlights() {
+        return flights;
+    }
+
+    public void setFlights(ArrayList<Flight> flights) {
+        this.flights = flights;
+    }
+
+    public ArrayList<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    public void setPassengers(ArrayList<Passenger> passengers) {
+        this.passengers = passengers;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
